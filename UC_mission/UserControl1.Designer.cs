acb95a69b4b8b341b59b46518d0c1aee06d7e26f
Sacha <sacha.ehrmann@etu.unistra.fr>
2025-05-07 14:34:35 +0200

    tableau de bord mis à jour selon la table et correction uc

--- UC_mission/UserControl1.Designer.cs
+++ UC_mission/UserControl1.Designer.cs
@@ -34,9 +34,9 @@
             this.lblCaserne = new System.Windows.Forms.Label();
             this.lblDescription = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnCloture = new System.Windows.Forms.Button();
             this.btnRapport = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -55,7 +55,7 @@
             // 
             this.lblType.AutoSize = true;
             this.lblType.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblType.Location = new System.Drawing.Point(186, 100);
+            this.lblType.Location = new System.Drawing.Point(186, 81);
             this.lblType.Name = "lblType";
             this.lblType.Size = new System.Drawing.Size(51, 22);
             this.lblType.TabIndex = 3;
@@ -75,7 +75,7 @@
             // 
             this.lblCaserne.AutoSize = true;
             this.lblCaserne.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCaserne.Location = new System.Drawing.Point(609, 30);
+            this.lblCaserne.Location = new System.Drawing.Point(561, 30);
             this.lblCaserne.Name = "lblCaserne";
             this.lblCaserne.Size = new System.Drawing.Size(93, 22);
             this.lblCaserne.TabIndex = 5;
@@ -85,7 +85,8 @@
             // 
             this.lblDescription.AutoSize = true;
             this.lblDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDescription.Location = new System.Drawing.Point(396, 100);
+            this.lblDescription.Location = new System.Drawing.Point(186, 110);
+            this.lblDescription.MaximumSize = new System.Drawing.Size(650, 100);
             this.lblDescription.Name = "lblDescription";
             this.lblDescription.Size = new System.Drawing.Size(33, 22);
             this.lblDescription.TabIndex = 6;
@@ -105,6 +106,16 @@
             this.panel1.Size = new System.Drawing.Size(853, 165);
             this.panel1.TabIndex = 7;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::UC_mission.Properties.Resources.gyrophare2;
+            this.pictureBox1.Location = new System.Drawing.Point(35, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(123, 102);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
             // btnCloture
             // 
             this.btnCloture.Location = new System.Drawing.Point(859, 2);
@@ -120,16 +131,7 @@
             this.btnRapport.Size = new System.Drawing.Size(91, 80);
             this.btnRapport.TabIndex = 9;
             this.btnRapport.UseVisualStyleBackColor = true;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::UC_mission.Properties.Resources.gyrophare2;
-            this.pictureBox1.Location = new System.Drawing.Point(35, 30);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(123, 102);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.btnRapport.Click += new System.EventHandler(this.btnRapport_Click);
             // 
             // Mission
             // 
