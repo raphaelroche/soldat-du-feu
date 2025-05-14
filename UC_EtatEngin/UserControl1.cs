520b145d30a8b4fd371433bbced697f702fa111a
BorZa-45 <abdoulaevr@gmail.com>
2025-05-14 08:39:39 +0200

    commit

--- UC_EtatEngin/UserControl1.cs
+++ UC_EtatEngin/UserControl1.cs
@@ -16,5 +16,15 @@ namespace UC_EtatEngin
         {
             InitializeComponent();
         }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
