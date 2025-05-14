0b0aaa54696aa362d54852fd5344cc027315e8e3
Sacha <sacha.ehrmann@etu.unistra.fr>
2025-05-14 10:21:10 +0200

    avancement du form 4

--- GestionPersonnel.Designer.cs
+++ GestionPersonnel.Designer.cs
@@ -30,6 +30,13 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.pnlMenu = new System.Windows.Forms.Panel();
+            this.pbIconePers = new System.Windows.Forms.PictureBox();
+            this.lblCaserne = new System.Windows.Forms.Label();
+            this.cbCasernes = new System.Windows.Forms.ComboBox();
+            this.cbPompiers = new System.Windows.Forms.ComboBox();
+            this.lblPompier = new System.Windows.Forms.Label();
+            this.pnlMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIconePers)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -43,18 +50,75 @@
             // pnlMenu
             // 
             this.pnlMenu.BackColor = System.Drawing.Color.DarkRed;
+            this.pnlMenu.Controls.Add(this.cbPompiers);
+            this.pnlMenu.Controls.Add(this.lblPompier);
+            this.pnlMenu.Controls.Add(this.cbCasernes);
+            this.pnlMenu.Controls.Add(this.lblCaserne);
+            this.pnlMenu.Controls.Add(this.pbIconePers);
             this.pnlMenu.Location = new System.Drawing.Point(12, 11);
             this.pnlMenu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pnlMenu.Name = "pnlMenu";
             this.pnlMenu.Size = new System.Drawing.Size(305, 800);
             this.pnlMenu.TabIndex = 2;
             // 
+            // pbIconePers
+            // 
+            this.pbIconePers.Image = global::SAE_A21D21_pompiers1.Properties.Resources.personnel;
+            this.pbIconePers.Location = new System.Drawing.Point(103, 15);
+            this.pbIconePers.Name = "pbIconePers";
+            this.pbIconePers.Size = new System.Drawing.Size(96, 89);
+            this.pbIconePers.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbIconePers.TabIndex = 0;
+            this.pbIconePers.TabStop = false;
+            // 
+            // lblCaserne
+            // 
+            this.lblCaserne.AutoSize = true;
+            this.lblCaserne.BackColor = System.Drawing.Color.Transparent;
+            this.lblCaserne.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCaserne.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblCaserne.Location = new System.Drawing.Point(31, 136);
+            this.lblCaserne.Name = "lblCaserne";
+            this.lblCaserne.Size = new System.Drawing.Size(197, 25);
+            this.lblCaserne.TabIndex = 1;
+            this.lblCaserne.Text = "Choisir une caserne :";
+            // 
+            // cbCasernes
+            // 
+            this.cbCasernes.FormattingEnabled = true;
+            this.cbCasernes.Location = new System.Drawing.Point(35, 166);
+            this.cbCasernes.Name = "cbCasernes";
+            this.cbCasernes.Size = new System.Drawing.Size(243, 24);
+            this.cbCasernes.TabIndex = 2;
+            this.cbCasernes.SelectedIndexChanged += new System.EventHandler(this.cbCasernes_SelectedIndexChanged);
+            // 
+            // cbPompiers
+            // 
+            this.cbPompiers.FormattingEnabled = true;
+            this.cbPompiers.Location = new System.Drawing.Point(35, 258);
+            this.cbPompiers.Name = "cbPompiers";
+            this.cbPompiers.Size = new System.Drawing.Size(243, 24);
+            this.cbPompiers.TabIndex = 4;
+            // 
+            // lblPompier
+            // 
+            this.lblPompier.AutoSize = true;
+            this.lblPompier.BackColor = System.Drawing.Color.Transparent;
+            this.lblPompier.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPompier.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblPompier.Location = new System.Drawing.Point(31, 228);
+            this.lblPompier.Name = "lblPompier";
+            this.lblPompier.Size = new System.Drawing.Size(186, 25);
+            this.lblPompier.TabIndex = 3;
+            this.lblPompier.Text = "Choisir un pompier :";
+            // 
             // GestionPersonnel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BackgroundImage = global::SAE_A21D21_pompiers1.Properties.Resources.pompier_fond;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(987, 783);
             this.Controls.Add(this.pnlMenu);
             this.Controls.Add(this.panel1);
@@ -62,6 +126,9 @@
             this.Name = "GestionPersonnel";
             this.Text = "Gestion du personnel";
             this.Load += new System.EventHandler(this.GestionPersonnel_Load);
+            this.pnlMenu.ResumeLayout(false);
+            this.pnlMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIconePers)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -70,5 +137,10 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel pnlMenu;
+        private System.Windows.Forms.PictureBox pbIconePers;
+        private System.Windows.Forms.Label lblCaserne;
+        private System.Windows.Forms.ComboBox cbCasernes;
+        private System.Windows.Forms.ComboBox cbPompiers;
+        private System.Windows.Forms.Label lblPompier;
     }
 }
--- GestionPersonnel.cs
+++ GestionPersonnel.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SAE_A21D21_pompiers;
 
 namespace SAE_A21D21_pompiers1
 {
     public partial class GestionPersonnel : Form
     {
+        private SQLiteConnection cx;
+
         public GestionPersonnel()
         {
             InitializeComponent();
@@ -20,7 +24,41 @@ namespace SAE_A21D21_pompiers1
 
         private void GestionPersonnel_Load(object sender, EventArgs e)
         {
-            pnlMenu.BackColor = Color.FromArgb(200, 139, 0, 0); //change l'opacité du menu            
+            cx = Connexion.Connec; // connexion à la base de données
+            pnlMenu.BackColor = Color.FromArgb(200, 139, 0, 0); // couleur du menu avec opacité
+
+            try
+            {
+                // Charger les casernes
+                string sqlCaserne = "SELECT * FROM Caserne";
+                SQLiteCommand comCaserne = new SQLiteCommand(sqlCaserne, cx);
+                SQLiteDataReader drCaserne = comCaserne.ExecuteReader();
+
+                while (drCaserne.Read())
+                {
+                    cbCasernes.Items.Add(drCaserne["nom"].ToString());
+                }
+            }
+            catch (SQLiteException err)
+            {
+                MessageBox.Show("Erreur lors du chargement des données :\n" + err.Message);
+            }
+
+        }
+
+        private void cbCasernes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            // Charger les pompiers
+            string sqlPompiers = "SELECT * FROM Pompier WHERE ";
+            SQLiteCommand comPompiers = new SQLiteCommand(sqlPompiers, cx);
+            SQLiteDataReader drPompiers = comPompiers.ExecuteReader();
+
+            while (drPompiers.Read())
+            {
+                cbPompiers.Items.Add(drPompiers["prenom"].ToString() + " " + drPompiers["nom"].ToString());
+            }
+            drPompiers.Close();
         }
     }
 }
