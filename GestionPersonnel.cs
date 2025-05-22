c80f0afd21a9d3fee79a0314a3b3c2138d650007
Sacha <sacha.ehrmann@etu.unistra.fr>
2025-05-22 11:59:28 +0200

    petites ameliorations

--- GestionPersonnel.cs
+++ GestionPersonnel.cs
@@ -54,7 +54,7 @@ namespace SAE_A21D21_pompiers1
 
             // Charger les pompiers
             string sqlPompiers = "SELECT * FROM Pompier p JOIN Affectation a ON p.matricule = a.matriculePompier " +
-                "WHERE a.idCaserne = " + (cbCasernes.SelectedIndex + 1);
+                "WHERE a.idCaserne = " + (cbCasernes.SelectedIndex + 1) + " and dateFin is NULL";
             SQLiteCommand comPompiers = new SQLiteCommand(sqlPompiers, cx);
             SQLiteDataReader drPompiers = comPompiers.ExecuteReader();
 
@@ -76,39 +76,40 @@ namespace SAE_A21D21_pompiers1
             if (cbPompiers.SelectedIndex != -1)
             {
 
-                
+
 
 
                 string sqlPompier = "SELECT * FROM Pompier WHERE prenom ='" + cbPompiers.SelectedItem.ToString().Split(' ')[0] + "'" +
                     "AND nom = '" + cbPompiers.SelectedItem.ToString().Split(' ')[1] + "';";
-                    SQLiteCommand comPompier = new SQLiteCommand(sqlPompier, cx);
-                    SQLiteDataReader drPompier = comPompier.ExecuteReader();
+                SQLiteCommand comPompier = new SQLiteCommand(sqlPompier, cx);
+                SQLiteDataReader drPompier = comPompier.ExecuteReader();
 
-                
+
 
 
 
                 AfficherPompier afficherPompier = null; // Initialisation de la variable
-                    while (drPompier.Read())
-                    {
+                while (drPompier.Read())
+                {
 
-                        string sqlGrade = "SELECT g.chaine FROM Grade g JOIN Pompier p ON g.code = p.codeGrade WHERE matricule = " + drPompier["matricule"];
+                    string sqlGrade = "SELECT g.chaine FROM Grade g JOIN Pompier p ON g.code = p.codeGrade WHERE matricule = " + drPompier["matricule"];
 
-                        SQLiteCommand comGrade = new SQLiteCommand(sqlGrade, cx);
-                        int chaineGrade = Convert.ToInt32(comGrade.ExecuteScalar());
+                    SQLiteCommand comGrade = new SQLiteCommand(sqlGrade, cx);
+                    int chaineGrade = Convert.ToInt32(comGrade.ExecuteScalar());
 
-                        afficherPompier = new AfficherPompier(Convert.ToInt32(drPompier["matricule"]), cbCasernes.SelectedItem.ToString(), drPompier["nom"].ToString(), drPompier["prenom"].ToString(), drPompier["sexe"].ToString(), drPompier["dateNaissance"].ToString(), drPompier["type"].ToString(), drPompier["dateEmbauche"].ToString(), 13-chaineGrade, drPompier["portable"].ToString(), drPompier["bip"].ToString(), Convert.ToInt32(drPompier["enConge"]) ,cx);
-                    }
-                    if (afficherPompier != null) // Vérification que la variable a été assignée
-                    {
+                    afficherPompier = new AfficherPompier(Convert.ToInt32(drPompier["matricule"]), cbCasernes.SelectedItem.ToString(), drPompier["nom"].ToString(), drPompier["prenom"].ToString(), drPompier["sexe"].ToString(), drPompier["dateNaissance"].ToString(), drPompier["type"].ToString(), drPompier["dateEmbauche"].ToString(), 13 - chaineGrade, drPompier["portable"].ToString(), drPompier["bip"].ToString(), Convert.ToInt32(drPompier["enConge"]), cx);
+                }
+                if (afficherPompier != null) // Vérification que la variable a été assignée
+                {
 
-                        afficherPompier.Location = new Point(10, 50); // Positionner le contrôle  
-                        afficherPompier.Visible = true; // S'assurer qu'il est visible  
-                        this.pnlAffichage.Controls.Add(afficherPompier);
-                        afficherPompier.BringToFront();
-                    }
+                    afficherPompier.Location = new Point(10, 50); // Positionner le contrôle  
+                    afficherPompier.Visible = true; // S'assurer qu'il est visible  
+                    this.pnlAffichage.Controls.Add(afficherPompier);
+                    afficherPompier.BringToFront();
+                }
                 drPompiers.Close();
             }
         }
+
     }
 }
