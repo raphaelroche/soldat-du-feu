3442d29aa15036b13fe246d3dfe323387ec7dbbb
raphael <raphael.roche@etu.unistra.fr>
2025-05-31 19:02:39 +0200

    finalisation de ajtpompier

--- FrmCreerPompier.cs
+++ FrmCreerPompier.cs
@@ -80,64 +80,72 @@ namespace SAE_A21D21_pompiers1
 
         private void btnValider_Click(object sender, EventArgs e)
         {
-            if (cboCaserne.SelectedIndex != -1 && cboGrade.SelectedIndex != -1 && cboSexe.SelectedIndex != -1 && cboStatus.SelectedIndex != -1 && txtBip.Text != String.Empty && txtMatricule.Text != String.Empty && txtNom.Text != String.Empty && txtPrenom.Text != String.Empty && txtTel.Text != String.Empty) 
+            if (cboCaserne.SelectedIndex != -1 && cboGrade.SelectedIndex != -1 && cboSexe.SelectedIndex != -1 && cboStatus.SelectedIndex != -1 && txtMatricule.Text != String.Empty && txtNom.Text != String.Empty && txtPrenom.Text != String.Empty && txtTel.Text != String.Empty) 
             {
-                SQLiteTransaction ajtPompier = cx.BeginTransaction();
-                SQLiteCommand com = new SQLiteCommand();
-                com.Connection = cx;
-                com.CommandType = CommandType.Text;
-                com.Transaction = ajtPompier;
-
-                string prenom = txtPrenom.Text;
-                string nom = txtNom.Text;
-                int matricule = Convert.ToInt32(txtMatricule.Text);
-                string sexe;
-                string status;
-                string dateNaissance = dtpNaissance.Text;
-                string portable = txtTel.Text;
-                int bip = Convert.ToInt32(txtBip.Text);
-                string codeGrade = "";
-                string sql = "select code from grade where libelle = '" + cboGrade.SelectedItem + "'";
-                string datEmbauche = DateTime.Now.ToString("yyyy-MM-dd");
-                int idCaserne = cboCaserne.SelectedIndex+1;
-
-                try
+                frmLogin log = new frmLogin(cx);
+                if(log.ShowDialog() == DialogResult.OK)
                 {
-                    SQLiteCommand c = new SQLiteCommand(sql, this.cx);
-                    string code = c.ExecuteScalar().ToString();
-                    codeGrade = code;
+                    SQLiteTransaction ajtPompier = cx.BeginTransaction();
+                    SQLiteCommand com = new SQLiteCommand();
+                    com.Connection = cx;
+                    com.CommandType = CommandType.Text;
+                    com.Transaction = ajtPompier;
 
+                    string prenom = txtPrenom.Text;
+                    string nom = txtNom.Text;
+                    int matricule = Convert.ToInt32(txtMatricule.Text);
+                    string sexe;
+                    string status;
+                    string dateNaissance = dtpNaissance.Text;
+                    string portable = txtTel.Text;
+                   
+                    string codeGrade = "";
+                    string sql = "select code from grade where libelle = '" + cboGrade.SelectedItem + "'";
+                    string datEmbauche = DateTime.Now.ToString("yyyy-MM-dd");
+                    int idCaserne = cboCaserne.SelectedIndex + 1;
+
+                    try
+                    {
+                        SQLiteCommand c = new SQLiteCommand(sql, this.cx);
+                        string code = c.ExecuteScalar().ToString();
+                        codeGrade = code;
+
+                    }
+                    catch (SQLiteException ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    sexe = cboSexe.SelectedIndex == 0 ? "m" : "f";
+                    status = cboStatus.SelectedIndex == 0 ? "p" : "v";
+
+
+                    try
+                    {
+                        com.CommandText = "insert into Pompier (matricule, nom, prenom, sexe, dateNaissance, type, portable, bip, enMission, enConge, codeGrade, dateEmbauche)" +
+                           $"values ({matricule},'{nom}','{prenom}','{sexe}','{dateNaissance}','{status}','{portable}',{matricule},0,0,'{codeGrade}','{datEmbauche}')";
+                        com.ExecuteNonQuery();
+
+                        com.CommandText = "insert into Affectation (matriculePompier, dateA, dateFin, idCaserne) values" +
+                            $"({matricule}, '{datEmbauche}', NULL,{idCaserne})";
+                        com.ExecuteNonQuery();
+
+                        ajtPompier.Commit();
+                        this.DialogResult = DialogResult.OK;
+
+
+
+                    }
+                    catch (SQLiteException ex)
+                    {
+                        ajtPompier.Rollback();
+                        MessageBox.Show("Ce matricule est deja attribué ! Veulliez en rentrer un nouveau.","pompier deja existant",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    }
                 }
-                catch (SQLiteException ex)
+                else
                 {
-                    MessageBox.Show(ex.Message);
+                    MessageBox.Show("Vous n'avez pas l'autorisation d'ajouter un pompier !");
                 }
-                sexe = cboSexe.SelectedIndex == 0 ? "m" : "f";
-                status = cboStatus.SelectedIndex == 0 ? "p" : "v";
-
-
-                try
-                {
-                    com.CommandText = "insert into Pompier (matricule, nom, prenom, sexe, dateNaissance, type, portable, bip, enMission, enConge, codeGrade, dateEmbauche)" +
-                       $"values ({matricule},'{nom}','{prenom}','{sexe}','{dateNaissance}','{status}','{portable}',{bip},0,0,'{codeGrade}','{datEmbauche}')";
-                    com.ExecuteNonQuery();
-
-                    com.CommandText = "insert into Affectation (matriculePompier, dateA, dateFin, idCaserne) values" +
-                        $"({matricule}, '{datEmbauche}', NULL,{idCaserne})";
-                    com.ExecuteNonQuery();
-
-                    ajtPompier.Commit();
-                    this.DialogResult = DialogResult.OK;
-                  
-
-
-                }
-                catch (SQLiteException ex)
-                {
-                    ajtPompier.Rollback();
-                    MessageBox.Show("Erreur, ajout du pompier impossible, penser a revérifier les informations que vous avez entré !");
-                    MessageBox.Show(ex.Message);
-                }
+               
 
 
 
