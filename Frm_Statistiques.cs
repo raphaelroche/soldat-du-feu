64d257ddbe62a85178295734f1503dc65615ef4f
BorZa-45 <abdoulaevr@gmail.com>
2025-06-05 10:09:03 +0200

    stat

---
+++ Frm_Statistiques.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SAE_A21D21_pompiers;
+using UC_EtatEngin;
+namespace SAE_A21D21_pompiers1
+{
+    public partial class Frm_Statistiques : Form
+    {
+
+        public Frm_Statistiques()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Statistiques_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+      
